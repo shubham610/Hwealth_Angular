085e40737cdf165ba964c5b8c2b0657a4391f16f
Hritik Mavi <Hritikchoudhary101@gmail.com>
2024-01-18 09:27:34 +0530

    Pulling Subham code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,LoginComponent,SignupComponent,AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
