a821f20c66d3c6efa8c8da989ad1f5f03864b3e1
Shubham Bansal <shubh.6102001@gmail.com>
2024-01-31 16:33:23 +0530

    Login/Signup Api use

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,23 +2,59 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink],
+  imports: [CommonModule,FormsModule,RouterOutlet,RouterLink,HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) {}
 
-  isLoggedIn(): boolean {
-    return this.authService.isLoggedIn();
+  formData:any={
+    userName:null,
+    userPassword:null
   }
-  login(): void {
-    // Assuming you get a token after successful login
-    const token = '1234';
-    this.authService.login(token);
+  loginUser(){
+    console.log(this.formData);
+    
+
+   this.logIn().subscribe( 
+    (response) => {console.log(response);
+      sessionStorage.setItem("token",response.userName)
+    },
+    (error) => {
+      console.log(error.error);
+    }
+   );
+    
   }
+
+  private apiUrl = 'http://localhost:8080/user';
+  constructor(private http: HttpClient) {}
+
+  private logIn():Observable<any>{ 
+    return this.http.post<any>(`${this.apiUrl}/login`, 
+      this.formData
+    );
+  }
+
+
+  // constructor(private authService: AuthService) {}
+
+  // isLoggedIn(): boolean {
+  //   return this.authService.isLoggedIn();
+  // }
+  // login(): void {
+  //   // Assuming you get a token after successful login
+  //   const token = '1234';
+  //   this.authService.login(token);
+  // }
+
+  
+
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,14 +1,46 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule,FormsModule,HttpClientModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  
+  formData:any={
+    userName:null,
+    userPhoneNo:null,
+    userPassword:null
 
-}
+  }
+  signUpUser(){
+    console.log(this.formData);
+    
+
+   this.signUp().subscribe( 
+    (response) => {console.log(response);
+      sessionStorage.setItem("token",response.userName)
+    },
+    (error) => {
+      console.log(error.error);
+    }
+   );
+    
+  }
+
+  private apiUrl = 'http://localhost:8080/user';
+  constructor(private http: HttpClient) {}
+
+  private signUp():Observable<any>{ 
+    return this.http.post<any>(`${this.apiUrl}/signup`, 
+      this.formData
+    );
+  }
+  }
